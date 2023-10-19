895e05980dff5aaf6862c1f4647d5a85e608c2a5
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-10-19 16:52:20 +0300

    added Catalog functionality

--- server/controllers/catalogController.js
+++ server/controllers/catalogController.js
@@ -2,12 +2,17 @@ const router = require("express").Router();
 const catalogManager = require("../managers/catalogManager");
 
 router.get("/", async (req, res) => {
+    
   try {
-    const catalog = await catalogManager.getAll();
+
+    const catalog = await catalogManager.getAll(req.query);
     res.json(catalog);
+    
   } catch (err) {
-
-
+    console.log(err);
+    res.status(400).json({
+      message: "Cannot create catalog",
+    });
   }
 });
 
@@ -25,6 +30,49 @@ router.post("/", async (req, res) => {
       message: "Cannot create catalog",
     });
   }
+});
+
+router.get("/:catalogId", async (req, res) => {
+    try { 
+    const catalog = await catalogManager.getOne(req.params.catalogId);
+
+    res.json(catalog);
+
+    } catch (err) {
+
+    console.log(err);
+    res.status(400).json({
+    message: "Cannot get Catalog post",
+    });
+}});
+
+router.put("/:catalogId", async (req, res) => {
+    try { 
+
+    await catalogManager.update(req.params.catalogId, req.body);
+
+    res.status(201).end();
+    } catch (err) {
+    console.log(err);
+    res.status(400).json({
+    message: "Cannot update Catalog post",
+    })};
+    
+});
+
+
+router.delete("/:catalogId", async (req, res) => {
+    try { 
+
+    await catalogManager.delete(req.params.catalogId);
+
+    res.status(201).end();
+    } catch (err) {
+    console.log(err);
+    res.status(400).json({
+    message: "Cannot delete Catalog post",
+    })};
+    
 });
 
 module.exports = router;
--- server/managers/catalogManager.js
+++ server/managers/catalogManager.js
@@ -1,6 +1,24 @@
 const Catalog = require("../models/Catalog");
 
-exports.getAll = () => Catalog.find();
+exports.getAll = async (qs) => {
+
+    let query = Catalog.find();
+
+    if(qs.where){
+        let [fieldName, ownerId] = qs.where.split("=");
+        ownerId = ownerId.replaceAll('"', '');
+        query = query.find({_ownerId: ownerId});
+    }
+    
+    const result = await query;
+
+    return result;
+}
+
+exports.getOne = (catalogId) => Catalog.findById(catalogId);
 
 exports.create = (catalogData) => Catalog.create(catalogData);
 
+exports.update = (catalogId, catalogData) => Catalog.findByIdAndUpdate(catalogId, catalogData);
+
+exports.delete = (catalogId) => Catalog.findByIdAndDelete(catalogId);
