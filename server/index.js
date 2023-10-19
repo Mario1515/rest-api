567ccd161fbbf182e90a6080f26231eda2af983d
Mario1515 <126467500+Mario1515@users.noreply.github.com>
2023-10-19 16:16:41 +0300

    User config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const cors  = require("cors");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const { auth } = require("./middlewares/authMiddleware");
 
 const app = express();
 
+mongoose.connect("mongodb://127.0.0.1:27017/catalog")
+    .then(() => console.log("DB Connect"))
+    .catch (err => console.log("Something went wrong " + err));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // parsing json data from client
+app.use(cors());
+app.use(auth);
+
 app.get("/", (req, res) => {
-   res.send("hello world!"); 
+  res.send("hello world!");
 });
 
+app.use(routes);
+
 app.listen(3030, () => {
-    console.log("RESTful server listening on port 3030...");
+  console.log("RESTful server listening on port 3030...");
 });
